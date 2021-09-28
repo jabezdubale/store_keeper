d42de0545bee4cc4be26a7ef22da305ae3a9983d
jabezdubale <jabezdubale6@gmail.com>
2021-09-28 06:27:07 +0300

    used @NotBlank and @Column(nullable = false) validations for important table fields

--- src/main/java/com/demo/store_keeper/domains/Employee_Info.java
+++ src/main/java/com/demo/store_keeper/domains/Employee_Info.java
@@ -2,27 +2,42 @@ package com.demo.store_keeper.domains;
 
 import lombok.Data;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @Data
 public class Employee_Info {
 
     @Id
+    @NotNull(message = "The employee's ID is required.")
+    @Column(nullable = false)
     private long EmployeeID;
 
+    @NotBlank(message = "The employee's First Name is required.")
+    @Column(nullable = false)
     private String FirstName;
 
+    @NotBlank(message = "The employee's Middle Name is required.")
+    @Column(nullable = false)
     private String MiddleName;
 
+    @NotBlank(message = "The employee's Last Name is required.")
+    @Column(nullable = false)
     private String LastName;
 
+    @NotBlank(message = "The employee's Role is required.")
+    @Column(nullable = false)
     private int EmployeeRole;
 
     private String Email;
 
-    private long Phone;
+    @NotBlank(message = "The employee's Phone Number is required.")
+    @Column(nullable = false)
+    private String Phone;
 
 
 }
